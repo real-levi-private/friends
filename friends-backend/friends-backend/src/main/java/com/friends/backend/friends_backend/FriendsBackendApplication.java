497e1ee9d63e2110c521ca9940baa3a037b8b63f
levi <otterbach.levi1@gmail.com>
2025-09-19 23:17:53 +0200

    Initial commit

---
+++ friends-backend/friends-backend/src/main/java/com/friends/backend/friends_backend/FriendsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tanks.backend.friends_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsBackendApplication.class, args);
+	}
+
+}
---
+++ friends-backend/friends-backend/src/test/java/com/friends/backend/friends_backend/FriendsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanks.backend.friends_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
